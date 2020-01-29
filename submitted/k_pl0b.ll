4368794223a1c2b49dce7892aca93dd36624d83f
tkrk1209 <raiki5100@gmail.com>
2020-01-30 00:22:56 +0900

    kadai5 ok

---
+++ submitted/k_pl0b.ll
@@ -0,0 +1,67 @@
+@.str1 = private unnamed_addr constant [3 x i8] c"%d\00", align 1
+@.str2 = private unnamed_addr constant [4 x i8] c"%d\0A\00", align 1
+@n = common global i32 0, align 4
+@x = common global i32 0, align 4
+
+define void @prime() #0 {
+  %1 = alloca i32, align 4
+  %2 = load i32, i32* @x, align 4
+  %3 = sdiv i32 %2, 2
+  store i32 %3, i32* %1, align 4
+  br label %4
+
+4:
+  %5 = load i32, i32* @x, align 4
+  %6 = load i32, i32* @x, align 4
+  %7 = load i32, i32* %1, align 4
+  %8 = sdiv i32 %6, %7
+  %9 = load i32, i32* %1, align 4
+  %10 = mul nsw i32 %8, %9
+  %11 = icmp ne i32 %5, %10
+  br i1 %11, label %12, label %15
+
+12:
+  %13 = load i32, i32* %1, align 4
+  %14 = sub nsw i32 %13, 1
+  store i32 %14, i32* %1, align 4
+  br label %4
+
+15:
+  %16 = load i32, i32* %1, align 4
+  %17 = icmp eq i32 %16, 1
+  br i1 %17, label %18, label %21
+
+18:
+  %19 = load i32, i32* @x, align 4
+  %20 = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([4 x i8], [4 x i8]* @.str2, i32 0, i32 0), i32 %19)
+  br label %21
+
+21:
+  ret void
+}
+
+define i32 @main() #0 {
+  %1 = alloca i32, align 4
+  store i32 0, i32* %1, align 4
+  %2 = call i32 (i8*, ...) @scanf(i8* getelementptr inbounds ([3 x i8], [3 x i8]* @.str1, i32 0, i32 0), i32* @n)
+  br label %3
+
+3:
+  %4 = load i32, i32* @n, align 4
+  %5 = icmp slt i32 1, %4
+  br i1 %5, label %6, label %10
+
+6:
+  %7 = load i32, i32* @n, align 4
+  store i32 %7, i32* @x, align 4
+  call void @prime()
+  %8 = load i32, i32* @n, align 4
+  %9 = sub nsw i32 %8, 1
+  store i32 %9, i32* @n, align 4
+  br label %3
+
+10:
+  ret i32 0
+}
+declare dso_local i32 @scanf(i8*, ...)
+declare dso_local i32 @printf(i8*, ...)
---
+++ submitted/tmp/code_generator.h
@@ -0,0 +1,199 @@
+#ifndef CODE_DATE
+#define CODE_DATE
+#include "data.h"
+
+/* 変数もしくは定数の型 */
+typedef struct
+{
+    Scope type;      /* 変数（のレジスタ）か整数の区別 */
+    char vname[256]; /* 変数の場合の変数名 */
+    int val;         /* 整数の場合はその値，変数の場合は割り当てたレジスタ番号 */
+} Factor;
+
+/* LLVM命令名の定義 */
+typedef enum
+{
+    // Common,   /* common */
+    Alloca,   /* alloca */
+    Store,    /* store  */
+    Load,     /* load   */
+    BrUncond, /* br     */
+    BrCond,   /* brc    */
+    Label,    /* label  */
+    Add,      /* add    */
+    Sub,      /* sub    */
+    Mult,     /* mult   */
+    Div,      /* div    */
+    Icmp,     /* icmp   */
+    Ret,       /* ret    */
+    Call
+} LLVMcommand;
+
+/* 比較演算子の種類 */
+typedef enum
+{
+    EQUAL, /* eq （==）*/
+    NE,    /* ne （!=）*/
+    SGT,   /* sgt （>，符号付き） */
+    SGE,   /* sge （>=，符号付き）*/
+    SLT,   /* slt （<，符号付き） */
+    SLE    /* sle （<=，符号付き）*/
+} Cmptype;
+
+/* 型の種類 */
+typedef enum
+{
+    I8,
+    I16,
+    I32,
+    VOID
+} Type;
+
+typedef struct llvmcode
+{
+    LLVMcommand command; /* 命令名 */
+    union {              /* 命令の引数 */
+        struct
+        { /* common */
+            Factor arg1;
+            Factor retval;
+        } common;
+        struct
+        { /* alloca */
+            Factor retval;
+        } alloca;
+        struct
+        { /* store  */
+            Factor arg1;
+            Factor arg2;
+        } store;
+        struct
+        { /* load   */
+            Factor arg1;
+            Factor retval;
+        } load;
+        struct
+        { /* br     */
+            int arg1;
+        } bruncond;
+        struct
+        { /* brc    */
+            Factor arg1;
+            int arg2;
+            int arg3;
+        } brcond;
+        struct
+        { /* label  */
+            int l;
+        } label;
+        struct
+        { /* add    */
+            Factor arg1;
+            Factor arg2;
+            Factor retval;
+        } add;
+        struct
+        { /* sub    */
+            Factor arg1;
+            Factor arg2;
+            Factor retval;
+        } sub;
+        struct
+        { /* mult   */
+            Factor arg1;
+            Factor arg2;
+            Factor retval;
+        } mult;
+        struct
+        { /* div    */
+            Factor arg1;
+            Factor arg2;
+            Factor retval;
+        } div;
+        struct
+        { /* icmp   */
+            Cmptype type;
+            Factor arg1;
+            Factor arg2;
+            Factor retval;
+        } icmp;
+        struct
+        { /* ret    */
+            Type rtype;
+            Factor arg1;
+        } ret;
+        struct
+        { /* call */
+            Type rtype;
+            Factor fname;
+            Factor args[10];
+            Factor retval;
+        } call;
+    } args;
+    /* 次の命令へのポインタ */
+    struct llvmcode *next;
+} LLVMcode;
+
+/* 変数もしくは定数のためのスタック */
+typedef struct
+{
+    Factor element[100]; /* スタック（最大要素数は100まで） */
+    unsigned int top;    /* スタックのトップの位置         */
+} Factorstack;
+
+Factorstack fstack; /* 整数もしくはレジスタ番号を保持するスタック */
+
+/* LLVMの関数定義 */
+typedef struct fundecl
+{
+    char fname[256];      /* 関数名                      */
+    unsigned arity;       /* 引数個数                    */
+    Factor args[10];      /* 引数名                      */
+    LLVMcode *codes;      /* 命令列の線形リストへのポインタ */
+    struct fundecl *next; /* 次の関数定義へのポインタ      */
+} Fundecl;
+
+extern LLVMcode *codehd; /* 命令列の先頭のアドレスを保持するポインタ */
+extern LLVMcode *codetl; /* 命令列の末尾のアドレスを保持するポインタ */
+
+/* 関数定義の線形リストの先頭の要素のアドレスを保持するポインタ */
+extern Fundecl *declhd;
+/* 関数定義の線形リストの末尾の要素のアドレスを保持するポインタ */
+extern Fundecl *decltl;
+
+typedef struct
+{
+    int element[100];
+    unsigned int top;
+} Labelstack;
+
+typedef struct
+{
+    LLVMcode *element[100];
+    unsigned int top;
+} BrAddstack;
+
+BrAddstack brstack;
+Labelstack lstack;
+
+void init_fstack();
+Factor factorpop();
+void factorpush(Factor x);
+LLVMcode *brpop();
+void brpush(LLVMcode *x);
+int labelpop();
+void labelpush(int x);
+// void backpatch();
+void generateCode(LLVMcommand command);
+void generateIcmp(Cmptype type);
+void generateCall(Type rtype, Factor fname, Factor args[10]);
+void displayFactor(Factor factor);
+void displayLlvmcodes(LLVMcode *code);
+void displayLlvmfundecl(Fundecl *decl);
+void insertDecl(char *fname, unsigned arity, Factor *args);
+Factor generateFactor(char *name);
+void displayGlobalVar();
+void insertCode(LLVMcode *tmp);
+// void displayfstack();
+
+#endif
